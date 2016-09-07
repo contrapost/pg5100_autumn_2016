4f7f5495a9ea3f88760f3fecbd00202eca53bd3c
arcuri82 <arcuri82@gmail.com>
2016-09-07 23:37:26 +0200

    time service example

---
+++ ejb/time/src/main/java/org/pg5100/ejb/time/businesslayer/CommentEJB.java
@@ -0,0 +1,34 @@
+package org.pg5100.ejb.time.businesslayer;
+
+import org.pg5100.ejb.time.datalayer.Comment;
+import org.pg5100.ejb.time.datalayer.News;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.constraints.NotNull;
+
+
+@Stateless
+public class CommentEJB {
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    /**
+        Create a comment to the News identified by {@code newsId}
+     */
+    public void save(@NotNull Long newsId, @NotNull String author, @NotNull String draft){
+
+        News news = em.find(News.class, newsId);
+        if(news == null){
+            return;
+        }
+
+        Comment comment = new Comment(null, draft, author);
+        em.persist(comment);
+
+        news.getComments().add(comment);
+    }
+}
