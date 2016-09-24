9435546157ab1a256de68b353826710c9c130b60
arcuri82 <arcuri82@gmail.com>
2016-09-24 18:22:56 +0200

    example for Session vs. Request scopes

---
+++ jsf/examples/src/main/java/org/pg5100/jsf/examples/ex04/RequestCounter.java
@@ -0,0 +1,10 @@
+package org.pg5100.jsf.examples.ex04;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class RequestCounter extends Counter {
+}
---
+++ jsf/examples/src/main/java/org/pg5100/jsf/examples/ex04/SessionCounter.java
@@ -0,0 +1,10 @@
+package org.pg5100.jsf.examples.ex04;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class SessionCounter extends Counter {
+}
